e92787f8fae1f6a1e4b6e6e111299e70c164b66e
TommyBoiss <165361500+TommyBoiss@users.noreply.github.com>
2024-07-02 22:00:29 +1200

    Update __init__.py

--- commonpasswords/src/commonpasswords/__init__.py
+++ commonpasswords/src/commonpasswords/__init__.py
@@ -1,38 +1,57 @@
 import re
-commonlayouts = [r'^[a-z]{3,}$', r'^[0-9a-z,./*\-+]{3,}$']
+commonlayouts = ["abc", "qwerty", "efg", "123", "456"]
+consecutiveletterspattern = r'([a-zA-Z0-9])\1\1'
 # abcdefghijklmnopqrstuvwxyz 1234567890qwertyuiopasdfghjklzxcvbnm,./*-+
 
 def detect(password:str, reason:bool = False):
     reasons = []
     points = 5
-    passwordlower = password.lower()
+    passwordlower = password.lower().strip()
     # CHECKING FOR COMMON LAYOUTS
+    alreadysetpatternsreasons = False
+    alreadysetpatternsadd = False
     for i in commonlayouts:
-        if re.match(i, passwordlower):
-            if reason == True:
-                reasons.append("Contains common combination")
-            points -= 1
-        else: 
-            print("added1")
-            points += 1
+            if re.match(i, passwordlower):
+                if reason == True and alreadysetpatternsreasons == False:
+                    reasons.append("Contains common combination")
+                    alreadysetpatternsreasons = True
+                points -= 1
+                alreadysetpatterns = True
+            else: 
+                if alreadysetpatternsadd == False:
+                    points += 1
+                    alreadysetpatternsadd = True
+            
 
     # CHECKING FOR COMMON WORDS
-    with open('src\commonpasswords\commonwords.txt', 'r') as file:
-        alreadysetcommonwords = False
+    with open('src/commonpasswords/commonwords.txt', 'r') as file:
+        alreadysetcommonwordsadd = False
+        alreadysetcommonwordsreasons = False
         for line in file:
-            print(line)
-            if alreadysetcommonwords == False:
-                if line in password:
-                    print(alreadysetcommonwords)
-                    if reason == True:
+                if line.strip() in passwordlower:
+                    if reason == True and alreadysetcommonwordsreasons == False:
                         reasons.append("Contains common words")
+                        alreadysetcommonwordsreasons = True
                     points -= 1
-                    alreadysetcommonwords = True
                 else: 
-                    points += 1
-                    alreadysetcommonwords = True
-            else:break
+                    if alreadysetcommonwordsadd == False:
+                        points += 1
+                        alreadysetcommonwordsadd = True
+
+    match = re.search(consecutiveletterspattern, passwordlower)
+    if match:
+        if reason == True:
+            reasons.append("Repetitive letters")
+        points -= 1
+        alreadysetcommonwords = True
+        
+    else: 
+        points += 1
+        alreadysetcommonwords = True
 
 
-    return points
+    if reason == True:
+        return [points, reasons]
+    else:
+        return points
     
