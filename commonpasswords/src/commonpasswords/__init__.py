9000304beef0dbd64cf352b38d77df203a210e0a
TommyBoiss <165361500+TommyBoiss@users.noreply.github.com>
2024-07-04 22:44:37 +1200

    Upgraded it to a class and started with configuration

--- commonpasswords/src/commonpasswords/__init__.py
+++ commonpasswords/src/commonpasswords/__init__.py
@@ -17,7 +17,7 @@ class main:
     self.commonlayoutreason = "Contains common combination"
 
     self.commonwordsenabled = True
-    self.commonwordpointsremove = 1
+    self.commonwordspointsremove = 1
     self.commonwordspointsadd = 0
     self.commonwordsreason = "Contains common words"
 
@@ -39,8 +39,25 @@ class main:
     self.charactertypesreason = "Only "+ str(self.charactertypesrequirement) +" type of Character"
 
  def _configure(self, configuration:dict):
-    self.startingnumber = configuration["startingpoints"]
-    
+    self.startingnumber = configuration.get("startingpoints", 0)
+    commonlayoutfolder = configuration.get("commonlayout")
+    if commonlayoutfolder != None: 
+        self.commonlayoutenabled = commonlayoutfolder.get("enabled", True)
+        self.commonlayoutpointsremove = commonlayoutfolder.get("pointsremove", 1)
+        self.commonlayoutpointsadd = commonlayoutfolder.get("pointsadd", 0)
+        self.commonlayoutreason = commonlayoutfolder.get("reason", "Contains common combination")
+
+    commonwordsfolder = configuration.get("commonlayout")
+    if commonwordsfolder != None: 
+        self.commonwordsenabled = commonwordsfolder.get("enabled", True)
+        self.commonwordspointsremove = commonwordsfolder.get("pointsremove", 1)
+        self.commonwordspointsadd = commonwordsfolder.get("pointsadd", 0)
+        self.commonwordsreason = commonwordsfolder.get("reason", "Contains common words")
+
+
+
+
+
 
 
 
@@ -74,7 +91,7 @@ class main:
                     if reason == True and alreadysetcommonwordsreasons == False:
                         reasons.append(self.commonwordsreason)
                         alreadysetcommonwordsreasons = True
-                    points -= self.commonwordpointsremove
+                    points -= self.commonwordspointsremove
                     alreadysetcommonwordsadd = True
                     break
         if alreadysetcommonwordsadd == True:
