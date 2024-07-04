89bf06fd4a331177471b816d2b59d58408b24d33
TommyBoiss <165361500+TommyBoiss@users.noreply.github.com>
2024-07-04 20:09:01 +1200

    Prepared for new configuration

--- commonpasswords/src/commonpasswords/__init__.py
+++ commonpasswords/src/commonpasswords/__init__.py
@@ -1,56 +1,124 @@
 import re
+import json
 commonlayouts = ["abc", "qwerty", "efg", "123", "456"]
 consecutiveletterspattern = r'([a-zA-Z0-9])\1\1'
+digitpattern = r"[0-9]"
+letterspattern = r"[a-zA-Z]"
+specialpattern = r'[!@#$%^&*()-+{}:"?/><,.;:]'
+
+startingnumber = 0
+
+commonlayoutenabled = True
+commonlayoutpointsremove = 1
+commonlayoutpointsadd = 0
+commonlayoutreason = "Contains common combination"
+
+commonwordsenabled = True
+commonwordpointsremove = 1
+commonwordspointsadd = 0
+commonwordsreason = "Contains common words"
+
+consecutivelettersenabled = True
+consecutivelettersremove = 1
+consecutivelettersadd = 0
+consecutivelettersreason = "Contains Repetitive letters"
+
+lengthenabled = True
+lengthrequirement = 8
+lengthremove = 1
+lengthadd = 0
+lengthreason = "Password is lower than " + str(lengthrequirement) + " characters"
+
+charactertypes = True
+charactertypesremove = 1
+charactertypesadd = 0
+charactertypesrequirement = 1
+charactertypesreason = "Only "+ str(charactertypesrequirement) +" type of Character"
+
 # abcdefghijklmnopqrstuvwxyz 1234567890qwertyuiopasdfghjklzxcvbnm,./*-+
+
+def configure(configuration:json):
+    print(configuration)
+
+
 
 def detect(password:str, reason:bool = False):
     reasons = []
-    points = 5
+    points = startingnumber
     passwordlower = password.lower().strip()
+
     # CHECKING FOR COMMON LAYOUTS
-    alreadysetpatternsreasons = False
-    alreadysetpatternsadd = False
-    for i in commonlayouts:
+    if commonlayoutenabled == True:
+        alreadysetpatternsreasons = False
+        for i in commonlayouts:
             if re.match(i, passwordlower):
                 if reason == True and alreadysetpatternsreasons == False:
-                    reasons.append("Contains common combination")
+                    reasons.append(commonlayoutreason)
                     alreadysetpatternsreasons = True
-                points -= 1
-                alreadysetpatterns = True
-            else: 
-                if alreadysetpatternsadd == False:
-                    points += 1
-                    alreadysetpatternsadd = True
+                points -= commonlayoutpointsremove
+            else:
+                    points += commonlayoutpointsadd
             
 
     # CHECKING FOR COMMON WORDS
-    with open('src/commonpasswords/commonwords.txt', 'r') as file:
-        alreadysetcommonwordsadd = False
+    if commonwordsenabled == True:
+     with open('src/commonpasswords/commonwords.txt', 'r') as file:
         alreadysetcommonwordsreasons = False
+        alreadysetcommonwordsadd = False
         for line in file:
                 if line.strip() in passwordlower:
                     if reason == True and alreadysetcommonwordsreasons == False:
-                        reasons.append("Contains common words")
+                        reasons.append(commonwordsreason)
                         alreadysetcommonwordsreasons = True
-                    points -= 1
-                else: 
-                    if alreadysetcommonwordsadd == False:
-                        points += 1
-                        alreadysetcommonwordsadd = True
+                    points -= commonwordpointsremove
+                    alreadysetcommonwordsadd = True
+                    break
+        if alreadysetcommonwordsadd == True:
+            points += commonwordspointsadd
+
+
 
 # Checking for consecutive letters
-    match = re.search(consecutiveletterspattern, passwordlower)
-    if match:
-        if reason == True:
-            reasons.append("Repetitive letters")
-        points -= 1
+    if consecutivelettersenabled == True:
+        match = re.search(consecutiveletterspattern, passwordlower)
+        if match:
+            if reason == True:
+                reasons.append(consecutivelettersreason)
+            points -= consecutivelettersremove
+        else:
+            points += consecutivelettersadd
         
-    else: 
-        points += 1
+# Check if less than 8 characters
+    if lengthenabled == True:
+        if len(password) < lengthrequirement:
+            points -= lengthremove
+            if reason == True:
+                reasons.append(lengthreason)
+        else:
+            points += lengthadd
 
+
+    if charactertypes == True:
+        CatagoryaCount = 0
+        if re.search(letterspattern, passwordlower):
+            CatagoryaCount += 1
+        if re.search(digitpattern, passwordlower):
+            CatagoryaCount += 1
+        if re.search(specialpattern, passwordlower):
+            CatagoryaCount += 1
+        if CatagoryaCount == charactertypesrequirement:
+            points -= charactertypesremove
+            if reason == True:
+                reasons.append(charactertypesreason)
+        else:
+            points += charactertypesadd
+
+         
+    
 
     if reason == True:
         return [points, reasons]
     else:
         return points
+    
     
