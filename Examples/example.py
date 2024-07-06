643397adaa0e963202aa5adc1628fffa25517eac
TommyBoiss <165361500+TommyBoiss@users.noreply.github.com>
2024-07-07 00:35:40 +1200

    Update and rename test to example.py

---
+++ Examples/example.py
@@ -0,0 +1,20 @@
+import commonpasswords
+commonpasswords.configure({ "startingpoints": 10,
+"commonlayout": {
+  "enabled": True,
+  "pointsremove": 5,
+  "pointsadd": 2,
+  "reason": "testing"
+},
+"commonwords": {
+  "enabled": True,
+  "pointsremove": 5,
+  "pointsadd": 2,
+  "reason": "test"
+}
+
+})
+def question():
+    print(commonpasswords.detect(input("Password\n"), True))
+    question()
+question()
