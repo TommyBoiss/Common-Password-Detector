e6ac85eedefdf434a0c51ad58952425434588a2f
TommyBoiss <165361500+TommyBoiss@users.noreply.github.com>
2024-07-06 22:34:31 +1200

    Update test_commonpasswords.py

--- tests/test_commonpasswords.py
+++ tests/test_commonpasswords.py
@@ -1,3 +1,3 @@
 import commonpasswords
 def test_detect():
-    assert commonpasswords.detect("password", reason=True) == [8, ['test', 'Only 1 type of Character']]
+    assert commonpasswords.detect("password", reason=True) == [-2, ['Contains common words', 'Only 1 type of Character']]
