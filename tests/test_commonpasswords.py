520ffe2a902e8db4eac4687115fa2c6284f6663f
TommyBoiss <165361500+TommyBoiss@users.noreply.github.com>
2024-07-06 23:30:48 +1200

    Update test_commonpasswords.py

--- tests/test_commonpasswords.py
+++ tests/test_commonpasswords.py
@@ -2,6 +2,6 @@ import commonpasswords
 def test_detect():
     assert commonpasswords.detect("password", reason=True) == [-1, ['Only 1 type of Character']]
     assert commonpasswords.detect("1923", reason=True) == [-3, ['Password is lower than 8 characters', 'Only 1 type of Character', 'Possibily contains a date']]
-    assert commonpasswords.detect("1") == -1
+    assert commonpasswords.detect("1") == -2
     assert commonpasswords.detect("qwerty") == [-3, ['Contains common combination', 'Password is lower than 8 characters', 'Only 1 type of Character']]
     
