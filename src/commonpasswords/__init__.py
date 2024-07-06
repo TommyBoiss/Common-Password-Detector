1fd3e73bf033dcac23a1699dfe1c7988338adef5
TommyBoiss <165361500+TommyBoiss@users.noreply.github.com>
2024-07-06 22:59:14 +1200

    Update __init__.py

--- src/commonpasswords/__init__.py
+++ src/commonpasswords/__init__.py
@@ -1,6 +1,5 @@
 import importlib.resources
 import re
-import importlib
 commonlayouts = ["qwerty", "qazwsxedcrfvtgbyhnujmik,ol.p;/"]
 consecutiveletterspattern = r'([a-zA-Z0-9])\1\1'
 digitpattern = r"[0-9]"
@@ -10,172 +9,167 @@ datepattern = r'\b(?:19|20)\d{2}(?!\d)'
 
 
 # abcdefghijklmnopqrstuvwxyz 1234567890qwertyuiopasdfghjklzxcvbnm,./*-+
-class main:
- def __init__(self):
-    self.startingnumber = 0
-    self.commonwordsdatabase = importlib.resources.files(__package__) / "commonwords.txt"
-
-    self.commonlayoutenabled = True
-    self.commonlayoutpointsremove = 1
-    self.commonlayoutpointsadd = 0
-    self.commonlayoutreason = "Contains common combination"
-    self.commonlayoutminimin = 3
-
-    self.commonwordsenabled = True
-    self.commonwordspointsremove = 1
-    self.commonwordspointsadd = 0
-    self.commonwordsreason = "Contains common words"
-
-    self.consecutivelettersenabled = True
-    self.consecutivelettersremove = 1
-    self.consecutivelettersadd = 0
-    self.consecutivelettersreason = "Contains Repetitive letters"
-
-    self.lengthenabled = True
-    self.lengthrequirement = 8
-    self.lengthremove = 1
-    self.lengthadd = 0
-    self.lengthreason = "Password is lower than " + str(self.lengthrequirement) + " characters"
-
-    self.charactertypesenabled = True
-    self.charactertypesremove = 1
-    self.charactertypesadd = 0
-    self.charactertypesrequirement = 1
-    self.charactertypesreason = "Only "+ str(self.charactertypesrequirement) +" type of Character"
-
-    self.dateenabled = True
-    self.dateremove = 1
-    self.dateadd = 0
-    self.datereason = "Possibily contains a date"
-
- def _configure(self, configuration:dict):
-    self.startingnumber = configuration.get("startingpoints", 0)
-    self.commonwordsdatabase = configuration.get("commonwordsdatabase", importlib.resources.files(__package__) / "commonwords.txt")
-    
-    commonlayoutfolder = configuration.get("commonlayout")
-    if commonlayoutfolder != None: 
-        self.commonlayoutenabled = commonlayoutfolder.get("enabled", True)
-        self.commonlayoutpointsremove = commonlayoutfolder.get("pointsremove", 1)
-        self.commonlayoutpointsadd = commonlayoutfolder.get("pointsadd", 0)
-        self.commonlayoutreason = commonlayoutfolder.get("reason", "Contains common combination")
-        self.commonlayoutreason = commonlayoutfolder.get("minimin", 3)
-
-    commonwordsfolder = configuration.get("commonwords")
-    if commonwordsfolder != None: 
-        self.commonwordsenabled = commonwordsfolder.get("enabled", True)
-        self.commonwordspointsremove = commonwordsfolder.get("pointsremove", 1)
-        self.commonwordspointsadd = commonwordsfolder.get("pointsadd", 0)
-        self.commonwordsreason = commonwordsfolder.get("reason", "Contains common words")
-
-    consecutivelettersfolder = configuration.get("consecutiveletters")
-    if consecutivelettersfolder != None: 
-        self.consecutivelettersenabled = consecutivelettersfolder.get("enabled", True)
-        self.consecutiveletterspointsremove = consecutivelettersfolder.get("pointsremove", 1)
-        self.consecutiveletterspointsadd = consecutivelettersfolder.get("pointsadd", 0)
-        self.consecutivelettersreason = consecutivelettersfolder.get("reason", "Contains Repetitive letters")
-        
-    lengthfolder = configuration.get("length")
-    if consecutivelettersfolder != None: 
-        self.lengthenabled = lengthfolder.get("enabled", True)
-        self.lengthrequirement = lengthfolder.get("requirement", 8)
-        self.lengthpointsremove = lengthfolder.get("pointsremove", 1)
-        self.lengthpointsadd = lengthfolder.get("pointsadd", 0)
-        self.lengthreason = lengthfolder.get("reason", "Password is lower than " + str(self.lengthrequirement) + " characters")
-
-    charactertypesfolder = configuration.get("charactertypes")
-    if consecutivelettersfolder != None: 
-        self.charactertypesenabled = charactertypesfolder.get("enabled", True)
-        self.charactertypesrequirement = charactertypesfolder.get("requirement", 8)
-        self.charactertypespointsremove = charactertypesfolder.get("pointsremove", 1)
-        self.charactertypespointsadd = charactertypesfolder.get("pointsadd", 0)
-        self.charactertypesreason = charactertypesfolder.get("reason", "Only "+ str(self.charactertypesrequirement) +" type of Character")
-
-        datefolder = configuration.get("date")
-    if consecutivelettersfolder != None: 
-        self.dateenabled = datefolder.get("enabled", True)
-        self.datepointsremove = datefolder.get("pointsremove", 1)
-        self.datepointsadd = datefolder.get("pointsadd", 0)
-        self.datereason = datefolder.get("reason", "Only "+ str(self.charactertypesrequirement) +" type of Character")
-
-
 
 
 
     # Finish off the code /: Have fun don't stay up lates
 
 
- def _detect(self, password:str, reason:bool = False):
+def detect(password:str, reason:bool = False, configuration:dict = {}):
     reasons = []
-    points = self.startingnumber
+    points = 0
     passwordlower = password.lower().strip()
+    commonwordsdatabase = importlib.resources.files(__package__) / "commonwords.txt"
+
+    commonlayoutenabled = True
+    ommonlayoutpointsremove = 1
+    commonlayoutpointsadd = 0
+    commonlayoutreason = "Contains common combination"
+    commonlayoutminimin = 3
+
+    commonwordsenabled = True
+    commonwordspointsremove = 1
+    commonwordspointsadd = 0
+    commonwordsreason = "Contains common words"
+
+    consecutivelettersenabled = True
+    consecutivelettersremove = 1
+    consecutivelettersadd = 0
+    consecutivelettersreason = "Contains Repetitive letters"
+
+    lengthenabled = True
+    lengthrequirement = 8
+    lengthremove = 1
+    lengthadd = 0
+    lengthreason = "Password is lower than " + str(lengthrequirement) + " characters"
+
+    charactertypesenabled = True
+    charactertypesremove = 1
+    charactertypesadd = 0
+    charactertypesrequirement = 1
+    charactertypesreason = "Only "+ str(charactertypesrequirement) +" type of Character"
+
+    dateenabled = True
+    dateremove = 1
+    dateadd = 0
+    datereason = "Possibily contains a date"
+    
+    points = configuration.get("startingpoints", 0)
+    commonwordsdatabase = configuration.get("commonwordsdatabase", importlib.resources.files(__package__) / "commonwords.txt")
+    
+    commonlayoutfolder = configuration.get("commonlayout")
+    if commonlayoutfolder != None: 
+        commonlayoutenabled = commonlayoutfolder.get("enabled", True)
+        commonlayoutpointsremove = commonlayoutfolder.get("pointsremove", 1)
+        commonlayoutpointsadd = commonlayoutfolder.get("pointsadd", 0)
+        commonlayoutreason = commonlayoutfolder.get("reason", "Contains common combination")
+        commonlayoutreason = commonlayoutfolder.get("minimin", 3)
+
+    commonwordsfolder = configuration.get("commonwords")
+    if commonwordsfolder != None: 
+        commonwordsenabled = commonwordsfolder.get("enabled", True)
+        commonwordspointsremove = commonwordsfolder.get("pointsremove", 1)
+        commonwordspointsadd = commonwordsfolder.get("pointsadd", 0)
+        commonwordsreason = commonwordsfolder.get("reason", "Contains common words")
+
+    consecutivelettersfolder = configuration.get("consecutiveletters")
+    if consecutivelettersfolder != None: 
+        consecutivelettersenabled = consecutivelettersfolder.get("enabled", True)
+        consecutiveletterspointsremove = consecutivelettersfolder.get("pointsremove", 1)
+        consecutiveletterspointsadd = consecutivelettersfolder.get("pointsadd", 0)
+        consecutivelettersreason = consecutivelettersfolder.get("reason", "Contains Repetitive letters")
+        
+    lengthfolder = configuration.get("length")
+    if consecutivelettersfolder != None: 
+        lengthenabled = lengthfolder.get("enabled", True)
+        lengthrequirement = lengthfolder.get("requirement", 8)
+        lengthpointsremove = lengthfolder.get("pointsremove", 1)
+        lengthpointsadd = lengthfolder.get("pointsadd", 0)
+        lengthreason = lengthfolder.get("reason", "Password is lower than " + str(lengthrequirement) + " characters")
+
+    charactertypesfolder = configuration.get("charactertypes")
+    if consecutivelettersfolder != None: 
+        charactertypesenabled = charactertypesfolder.get("enabled", True)
+        charactertypesrequirement = charactertypesfolder.get("requirement", 8)
+        charactertypespointsremove = charactertypesfolder.get("pointsremove", 1)
+        charactertypespointsadd = charactertypesfolder.get("pointsadd", 0)
+        charactertypesreason = charactertypesfolder.get("reason", "Only "+ str(charactertypesrequirement) +" type of Character")
+
+        datefolder = configuration.get("date")
+    if consecutivelettersfolder != None: 
+        dateenabled = datefolder.get("enabled", True)
+        datepointsremove = datefolder.get("pointsremove", 1)
+        datepointsadd = datefolder.get("pointsadd", 0)
+        datereason = datefolder.get("reason", "Only "+ str(charactertypesrequirement) +" type of Character")
+
 
     # CHECKING FOR COMMON LAYOUTS
-    if self.commonlayoutenabled == True:
+    if commonlayoutenabled == True:
         alreadysetpatternsreasons = False
         alreadysetpatternsadd = False
         for icommon in commonlayouts:
           
           for i in range(len(icommon) - 2):
-            substr = icommon[i:i+self.commonlayoutminimin-1]
+            substr = icommon[i:i+commonlayoutminimin-1]
 
             if substr in passwordlower:
              if alreadysetpatternsreasons == False:
                 if reason == True:
-                    reasons.append(self.commonlayoutreason)
+                    reasons.append(commonlayoutreason)
 
-                points -= self.commonlayoutpointsremove
+                points -= commonlayoutpointsremove
                 alreadysetpatternsreasons = True
                 
 
             else:
                     if alreadysetpatternsadd == False:
-                        points += self.commonlayoutpointsadd
+                        points += commonlayoutpointsadd
                         alreadysetpatternsadd = True
 
         
             
 
     # CHECKING FOR COMMON WORDS
-    if self.commonwordsenabled == True:
-     with open(self.commonwordsdatabase, 'r') as file:
+    if commonwordsenabled == True:
+     with open(commonwordsdatabase, 'r') as file:
         alreadysetcommonwordsreasons = False
         alreadysetcommonwordsadd = False
         for line in file:
                 if line.strip() in passwordlower:
 
                     if reason == True and alreadysetcommonwordsreasons == False:
-                        reasons.append(self.commonwordsreason)
+                        reasons.append(commonwordsreason)
                         alreadysetcommonwordsreasons = True
-                    points -= self.commonwordspointsremove
+                    points -= commonwordspointsremove
                     break
                 else:
                     if alreadysetcommonwordsadd == False:
-                        points += self.commonwordspointsadd
+                        points += commonwordspointsadd
                         alreadysetcommonwordsadd = True
 
 
 
 # Checking for consecutive letters
-    if self.consecutivelettersenabled == True:
+    if consecutivelettersenabled == True:
         match = re.search(consecutiveletterspattern, passwordlower)
         if match:
             if reason == True:
-                reasons.append(self.consecutivelettersreason)
-            points -= self.consecutivelettersremove
+                reasons.append(consecutivelettersreason)
+            points -= consecutivelettersremove
         else:
-            points += self.consecutivelettersadd
+            points += consecutivelettersadd
         
 # Check if less than 8 characters
-    if self.lengthenabled == True:
-        if len(password) < self.lengthrequirement:
-            points -= self.lengthremove
+    if lengthenabled == True:
+        if len(password) < lengthrequirement:
+            points -= lengthremove
             if reason == True:
-                reasons.append(self.lengthreason)
+                reasons.append(lengthreason)
         else:
-            points += self.lengthadd
+            points += lengthadd
 
 # Detects for more than a certain amount of text catagories
-    if self.charactertypesenabled == True:
+    if charactertypesenabled == True:
         CatagoryaCount = 0
         if re.search(letterspattern, passwordlower):
             CatagoryaCount += 1
@@ -183,20 +177,20 @@ class main:
             CatagoryaCount += 1
         if re.search(specialpattern, passwordlower):
             CatagoryaCount += 1
-        if CatagoryaCount == self.charactertypesrequirement:
-            points -= self.charactertypesremove
+        if CatagoryaCount == charactertypesrequirement:
+            points -= charactertypesremove
             if reason == True:
-                reasons.append(self.charactertypesreason)
+                reasons.append(charactertypesreason)
         else:
-            points += self.charactertypesadd
+            points += charactertypesadd
 
-    if self.dateenabled == True:
+    if dateenabled == True:
      if re.search(datepattern, password):
-        points -= self.dateremove
+        points -= dateremove
         if reason == True:
-            reasons.append(self.datereason)
+            reasons.append(datereason)
      else: 
-        points += self.dateadd
+        points += dateadd
         
     
 
@@ -207,10 +201,4 @@ class main:
         return [points, reasons]
     else:
         return points
-    
-mainclass = main()
-def configure(configuration:dict):
-    return mainclass._configure(configuration)
-def detect(password:str, reason:bool = False):
-    return mainclass._detect(password, reason)
     
